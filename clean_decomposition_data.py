0941385b5122c0adb4691e5ad49fb1d558e3c39b
louisethomas <louiseerinthomas@gmail.com>
2020-06-20 00:50:53 +0100

    remove unused files

--- clean_decomposition_data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('DATA_FILES/CharacterDecompositionWiki.csv', sep='\t', index_col='Character')
-df = df.loc[:,['Strokes','Composition','Character1','Strokes1','Verification1','Character2','Strokes2','Verification2','Cangjie','Radical']]
-df.dropna(how='all', inplace=True)
-
-print(df)
-df.to_csv('DATA_FILES/CharacterDecompositionWiki_cleaned.csv')
